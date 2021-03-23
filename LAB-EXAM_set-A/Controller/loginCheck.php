12de090ed7d723d8cdf57c2888f786b3fc8f2191
AhmedSajjad <sajjadah1952@gmail.com>
2021-03-23 09:20:02 +0600

    Lab 2

--- LAB-EXAM_set-A/Controller/loginCheck.php
+++ LAB-EXAM_set-A/Controller/loginCheck.php
@@ -1,31 +1,31 @@
 <?php
 
-	session_start();
+    session_start();
 
-	if(isset($_POST['submit'])){
-        $users = file_get_contents('../Model/users.json');
+    if(isset($_POST['submit'])){
+        $users = file_get_contents('../Model/user.json');
         $users = json_decode($users);
 
 
-		$username = $_POST['email'];
-		$password = $_POST['password'];
+        $username = $_POST['email'];
+        $password = $_POST['password'];
 
-		if($username == "" || $password == ""){
-			echo "null input...";
-		}else{
+        if($username == "" || $password == ""){
+            echo "null input...";
+        }else{
 
 
             foreach($users as $user)
             {
                 if($user->email == $username && $user->password == $password && $user->role == "admin"){
-                    //echo "Logged in";
+                    
                     $_SESSION['flag'] = true;
-                    header('location: ../View/AdminDashboard.html');
+                    header('location: ../View/adminDashboard.php');
                     break;
                 }
                 else if($user->email == $username && $user->password == $password && $user->role == "admin"){
                     $_SESSION['flag'] = true;
-                    header('location: ../View/UserDashboard.html');
+                    header('location: ../View/userDashboard.php');
                     break;
                 }
                 
@@ -36,9 +36,9 @@
                 }
             }
 
-			
-		}
-	}
+            
+        }
+    }
 
 
 ?>
