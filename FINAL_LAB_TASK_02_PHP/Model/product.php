d250647c9856edd8efef54f544aee61f397bdb8c
AhmedSajjad <sajjadah1952@gmail.com>
2021-03-23 10:53:40 +0600

    Final Commit

--- FINAL_LAB_TASK_02_PHP/Model/product.php
+++ FINAL_LAB_TASK_02_PHP/Model/product.php
@@ -3,19 +3,6 @@
     function insertProduct($product)
     {
 	$conn = getConnection();
-    $sql = "insert into products values('', '{$product['name']}', '{$product['buyingprice']}', '{$product['sellingprice']}', '{$product['displayable']}')";
-	$result = mysqli_query($conn, $sql);
-
-	if($result){
-		return true;
-	}
-    else
-    {
-		return false;<?php 
-    require_once('db.php');
-    function insertProduct($product)
-    {
-	$conn = getConnection();
     $sql = "insert into products values('', '{$product['name']}', '{$product['buyingprice']}', '{$product['sellingprice']}','{$product['profit']}','')";
 	$result = mysqli_query($conn, $sql);
 
@@ -41,22 +28,16 @@
 	}else{
 		return false;
 	}
-}
+    }
 
-	}
-
+    function getProductbyName($name)
+    {
+	$conn = getConnection();
+	$sql = "select * from products where name='{$name}'";
+	$result = mysqli_query($conn, $sql);
+	$row = mysqli_fetch_assoc($result);
+	return $row;
     }
 
 
-    function deleteProduct($id){
-	$conn = getConnection();
-	$sql = "delete from products where id='{$id}";
-	$result = mysqli_query($conn, $sql);
-	
-	if($result){
-		return true;
-	}else{
-		return false;
-	}
-}
 ?>
