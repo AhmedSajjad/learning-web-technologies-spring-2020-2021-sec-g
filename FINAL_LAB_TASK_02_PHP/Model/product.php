c8e38aacf7241be083f3a52da9f0c3fefaf33faa
AhmedSajjad <sajjadah1952@gmail.com>
2021-03-23 10:03:27 +0600

    2ns push

--- FINAL_LAB_TASK_02_PHP/Model/product.php
+++ FINAL_LAB_TASK_02_PHP/Model/product.php
@@ -11,7 +11,38 @@
 	}
     else
     {
+		return false;<?php 
+    require_once('db.php');
+    function insertProduct($product)
+    {
+	$conn = getConnection();
+    $sql = "insert into products values('', '{$product['name']}', '{$product['buyingprice']}', '{$product['sellingprice']}','{$product['profit']}','')";
+	$result = mysqli_query($conn, $sql);
+
+	if($result)
+    {
+		return true;
+	}
+    else
+    {
 		return false;
+	}
+
+    }
+
+
+    function deleteProduct($id){
+	$conn = getConnection();
+	$sql = "delete from products where id='{$id}";
+	$result = mysqli_query($conn, $sql);
+	
+	if($result){
+		return true;
+	}else{
+		return false;
+	}
+}
+
 	}
 
     }
